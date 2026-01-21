ade6b70514082e7bbb7d308208ac1dfebf20ac05
jsbmomo <tytyes40@gmail.com>
2026-01-21 17:14:48 +0900

    :sparkles: Logback 및 Intercepter 로그 설정(추가 수정 필요)

---
+++ src/main/java/com/stock/forecast_service/common/api/LoggingInterceptor.java
@@ -0,0 +1,52 @@
+package com.stock.forecast_service.common.api;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+    if (request.getParameterNames().hasMoreElements()) {
+      log.info("Request Method: [{}] URL: [{}] Params: [{}]",request.getMethod(), request.getRequestURI(), getRequestParams(request));
+    } else {
+      log.info("Request Method: [{}] URL: [{}]",request.getMethod(), request.getRequestURI());
+    }
+
+    return true;
+  }
+
+  @Override
+  public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+  }
+
+  @Override
+  public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    log.info("Response Status : [{}]", response.getStatus());
+  }
+
+  private Map<String, String> getRequestParams(HttpServletRequest request) {
+    Map<String, String> paramMap = new HashMap<>();
+    Enumeration<String> parameterNames = request.getParameterNames();
+
+    while (parameterNames.hasMoreElements()) {
+      String paramName = parameterNames.nextElement();
+      paramMap.put(paramName, request.getParameter(paramName));
+    }
+
+    return paramMap;
+  }
+
+
+}
